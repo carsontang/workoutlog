7241c9faf354ede29bfbf0ce41d183a9a85dab89
Carson Tang <tang.carson@gmail.com>
2013-04-03 00:21:32 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Workoutlog::Application.config.secret_token = '0d1000f67ad985367b20a03a0cb782b075c23805777e8d507717be168ceef171878f15d0c79ecf910983c62541f033b20dbb0a11d642f8d243fa7fd874d37b46'
