4f98202aac246c8781d3bf36591c707f5468b95b
Carson Tang <tang.carson@gmail.com>
2013-04-11 13:40:53 -0700

    added timepicker; timepicker doesn't combine with datepicker

--- app/assets/javascripts/workouts/_create.js
+++ app/assets/javascripts/workouts/_create.js
@@ -1,5 +1,6 @@
 WO.workouts.create = {
   init: function() {
     $("#workout_workout_date").datepicker({'dateFormat': 'DD MM dd, yy'});
+    $("#workout_workout_date_time").timepicker({ 'scrollDefaultNow': true });
   }
 };
