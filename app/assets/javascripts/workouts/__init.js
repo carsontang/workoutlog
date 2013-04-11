333a643792c4e2f9ec62301e040ed5b60a6fefe1
Carson Tang <tang.carson@gmail.com>
2013-04-11 10:38:44 -0700

    Added jQuery UI Datepicker to workouts#new

--- app/assets/javascripts/workouts/__init.js
+++ app/assets/javascripts/workouts/__init.js
@@ -1,5 +1,6 @@
 WO.workouts = {
   init: function() {
     WO.workouts.index.init();
+    WO.workouts.create.init();
   }
 }
---
+++ app/assets/javascripts/workouts/_create.js
@@ -0,0 +1,5 @@
+WO.workouts.create = {
+  init: function() {
+    $("#workout_workout_date").datepicker({'dateFormat': 'yy-mm-dd'});
+  }
+};
