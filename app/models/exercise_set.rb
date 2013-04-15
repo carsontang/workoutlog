26e79ac1ae3bf142fed7b5515f5a716f87674a15
Carson Tang <tang.carson@gmail.com>
2013-04-15 08:08:40 -0700

    Added server side data validations for ExerciseSet

--- app/models/exercise_set.rb
+++ app/models/exercise_set.rb
@@ -1,4 +1,13 @@
 class ExerciseSet < ActiveRecord::Base
   belongs_to :exercise
   attr_accessible :number_of_reps, :number_of_sets, :weight
+
+  validates :number_of_reps, :presence => true,
+            :numericality => { :greater_than_or_equal_to => 0 }
+
+  validates :number_of_sets, :presence => true,
+            :numericality => { :greater_than_or_equal_to => 0 }
+            
+  validates :weight, :presence => true,
+            :numericality => { :greater_than_or_equal_to => 0 }
 end
