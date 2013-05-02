f0030e0c6350111e675757bdf44a3df1e73aa6d7
Carson Tang <tang.carson@gmail.com>
2013-05-01 23:13:24 -0700

    Fixed bug that prevented user from seeing a workout entry when a calendar workout entry is clicked; refactored WorkoutsController#create

--- app/assets/javascripts/workouts/_index.js
+++ app/assets/javascripts/workouts/_index.js
@@ -1,6 +1,11 @@
 WO.workouts.index = {
   init: function() {
-    $('.calendar td').click(function(){
+    $('.calendar td').click(function(evt){
+
+      if (evt.target !== this) {
+        // clicked on a descendant, forward user
+        return;
+      }
       var date = $(this).find(".date-info").text();
       window.location.replace("/workouts/new?date=" + date);
     });
--- app/controllers/workouts_controller.rb
+++ app/controllers/workouts_controller.rb
@@ -59,12 +59,9 @@ class WorkoutsController < ApplicationController
     # TODO: Clean up this params mess and make sure workouts#update allows multiple lift edit
     # TODO: Editing workouts should allow you to update workouts
     # TODO: Switch times to local time
-    params[:workout][:workout_date] = "#{params[:workout][:workout_date]} #{params[:workout][:time]}"
-    params[:workout] = params[:workout].slice(:workout_date, :exercises_attributes)
-    params[:workout][:exercises_attributes].each do |id, lift_name|
-      lift_name = params[:workout][:exercises_attributes]["#{id}"][:lift_id]
-      params[:workout][:exercises_attributes]["#{id}"][:lift_id] = Lift.find_by_lift_name(lift_name).id
-    end
+    set_workout_date(params)
+    remove_workout_time(params)
+    set_lift_ids(params)
     
     @workout = current_user.workouts.build(params[:workout])
 
@@ -110,5 +107,29 @@ class WorkoutsController < ApplicationController
       format.html { redirect_to workouts_url }
       format.json { head :no_content }
     end
+  end
+
+  private
+
+  def set_lift_id(id, params)
+    lift_name = params[:workout][:exercises_attributes]["#{id}"][:lift_id]
+    params[:workout][:exercises_attributes]["#{id}"][:lift_id] = Lift.find_by_lift_name(lift_name).id
+  end
+
+  def set_lift_ids(params)
+    params[:workout][:exercises_attributes].each do |id, lift_name|
+      set_lift_id(id, params)
+    end
+  end
+
+  # A little hack to combine inputs from jQuery datepicker and timepicker
+  def set_workout_date(params)
+    params[:workout][:workout_date] = "#{params[:workout][:workout_date]} #{params[:workout][:time]}"
+  end
+
+  # Because Workout does not include :time as an attribute,
+  # params[:workout] must remove :time
+  def remove_workout_time(params)
+    params[:workout] = params[:workout].slice(:workout_date, :exercises_attributes)
   end
 end
